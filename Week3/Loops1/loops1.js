a1f36f354dea1cefa1b427a2a9151dd98d3ffb3c
Hannah <hannahsvendor@gmail.com>
2021-07-12 14:42:07 -0400

    Updated

---
+++ Week3/Loops1/loops1.js
@@ -0,0 +1,31 @@
+for(let a = 1; a <= 10; a++){
+     console.log(a); //1-10  
+}
+
+for(let b = 10; b >= 1; b--){
+    console.log(b); //10-1
+}
+
+let c = 1
+while (c <= 10){
+    console.log(c);
+    c++;
+}
+
+let d = 10
+while (d >= 1){
+    console.log(d);
+    d--;
+}
+
+let e = 1
+do {
+    console.log(e);
+    e++;
+} while (e <= 10);
+
+let f = 10
+do {
+    console.log(f);
+    f--;
+} while (f >= 1);
---
+++ Week3/Refresher/refresher.js
@@ -0,0 +1,23 @@
+const firstName="Hannah"; //Hannah
+const lastName="Svendor"; //Svendor
+const name= firstName + " " + lastName;
+console.log(name);
+
+let population="198,401"
+console.log(population);
+
+population=population * 3;
+console.log(population);
+
+
+let javaScriptIsCool = true;
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
